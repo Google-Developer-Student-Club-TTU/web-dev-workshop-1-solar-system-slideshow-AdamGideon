1920c4827ca352cb12c0394259ff59adf4a8a54b
AdamGideon <152444959+AdamGideon@users.noreply.github.com>
2023-11-29 15:30:23 -0600

    Add files via upload

---
+++ Web Dev/planets.js
@@ -0,0 +1,89 @@
+// planets.js
+
+const planets = [
+    {
+      name: "Mercury",
+      mass: "Mass: 0.33 Earths",
+      position: 1,
+      hasRings: "Rings: No",
+      image: "images/mercury.jpeg",
+      description: "Mercury is the sped little planet closest to the Sun."
+    },
+    {
+      name: "Venus",
+      mass: "Mass 0.82 Earths",
+      position: 2,
+      hasRings: "Rings: No",
+      image: "images/venus.jpeg",
+      description: "Venus is Earth's hot and toxic sister, with a runaway greenhouse effect."
+    },
+    {
+      name: "Earth",
+      mass: "Mass: 1 Earth",
+      position: 3,
+      hasRings: "Rings: No",
+      image: "images/earth.jpg",
+      description: "Earth is the only known planet to support life. Don't listen to this crap, it is not true this is all a simulation. The Earth is flat, save up on gold, sell your land!"
+    },
+    {
+      name: "Mars",
+      mass: "Mass: 0.11 Earths",
+      position: 4,
+      hasRings: "Rings: No",
+      image: "images/mars.jpeg",
+      description: "Mars, the 'Red Planet,' awaits future human explorers."
+    },
+    {
+      name: "Jupiter",
+      mass: "Mass: 317.83 Earths",
+      position: 5,
+      hasRings: "Rings: Yes",
+      image: "images/jupiter.jpeg",
+      description: "Jupiter is the big gas giant with a swirling storm called the Great Red Spot."
+    },
+    {
+      name: "Saturn",
+      mass: "Mass: 95 Earths",
+      position: 6,
+      hasRings: "Rings: Yes",
+      image: "images/saturn.jpeg",
+      description: "Saturn is famous for its stunning rings. It's basically the solar system's jewelry box."
+    },
+    {
+      name: "Uranus",
+      mass: "Mass: 14.54 Earths",
+      position: 7,
+      hasRings: "Rings: Yes",
+      image: "images/uranus.jpeg",
+      description: "Uranus is tilted on its side, making it the solar system's misfit."
+    },
+    {
+      name: "Neptune",
+      mass: "Mass: 17.15 Earths",
+      position: 8,
+      hasRings: "Rings: Yes",
+      image: "images/neptune.jpeg",
+      description: "Neptune, the icy giant, is far, far away in the deep, dark, and chilly reaches of the solar system."
+    },
+    {
+      name: "Pluto",
+      mass: "Mass: 0.002 Earths",
+      position: "Dwarf Planet",
+      hasRings: "Rings: No",
+      image: "images/pluto.jpeg",
+      description: "Pluto, once the ninth planet, got demoted. It's basically the rebel of the solar system, doing its own thing."
+    }
+  ];
+  
+  export default planets;
+  
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
